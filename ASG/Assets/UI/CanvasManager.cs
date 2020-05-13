580b6336e7e4fdaa381772b6db095a90e2050028
tbrierley <57731355+tbrierley@users.noreply.github.com>
2020-05-13 14:38:43 -0400

    UI Updating

---
+++ ASG/Assets/UI/CanvasManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    private static CanvasManager instanceRef;
+
+    void Awake()
+    {
+        if (instanceRef == null)
+        {
+            instanceRef = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+}
---
+++ ASG/Assets/UI/RenderCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCamera : MonoBehaviour
+{
+    private static RenderCamera instanceRef;
+
+    void Awake()
+    {
+        if (instanceRef == null)
+        {
+            instanceRef = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+}
